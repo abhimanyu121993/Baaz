3e8b5149f213ac64c914d849d37500181d3f8a0d
KillerHeman <sharmahcool5@gmail.com>
2022-08-05 18:15:32 +0530

    hisu

--- resources/views/Backend/orderhistory.blade.php
+++ resources/views/Backend/orderhistory.blade.php
@@ -92,8 +92,8 @@
                                                 @php $oid=Crypt::encrypt($order->order_id); @endphp
                                                 <td><a href="{{ route('Backend.orderhistory.show', $oid) }}">BAAZ-{{ $order->order_id }}</a></td>
                                                 <td>{{ $order->user->name ?? 'BAAZ Customer' }}</td>
-                                                <td>{{ $order->slot ?? '' }}</td>
-                                                <td>{{ $order->user->mobileno ?? '' }}</td>
+                                                <td>{{ $order->slot ?? ''}}</td>
+                                                <td>{{ $order->user->mobileno ?? ''}}</td>
                                                 <td>{{ $order->total_amount }}</td>
                                                 <td>{{ $order->order_status }}</td>
                                                 <td>{{ $order->payment_mode }}</td>
