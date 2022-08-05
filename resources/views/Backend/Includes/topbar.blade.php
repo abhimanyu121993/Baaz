865f2a040a89353671ea9e4d033e7e2741209cf9
KillerHeman <sharmahcool5@gmail.com>
2022-08-05 21:03:07 +0530

    hisu

--- resources/views/Backend/Includes/topbar.blade.php
+++ resources/views/Backend/Includes/topbar.blade.php
@@ -1,39 +1,55 @@
-<nav class="header-navbar navbar navbar-expand-lg align-items-center floating-nav navbar-light navbar-shadow container-xxl">
+<nav
+    class="header-navbar navbar navbar-expand-lg align-items-center floating-nav navbar-light navbar-shadow container-xxl">
     <div class="navbar-container d-flex content">
-      <div class="bookmark-wrapper d-flex align-items-center">
-        <ul class="nav navbar-nav d-xl-none">
-          <li class="nav-item"><a class="nav-link menu-toggle" href="#"><i class="ficon" data-feather="menu"></i></a></li>
-        </ul>
-        <ul class="nav navbar-nav bookmark-icons">
-          <li class="nav-item d-none d-lg-block"><a class="nav-link" href="#" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Email"><i class="ficon" data-feather="mail"></i></a></li>
-          <li class="nav-item d-none d-lg-block"><a class="nav-link" href="#" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Chat"><i class="ficon" data-feather="message-square"></i></a></li>
-          <li class="nav-item d-none d-lg-block"><a class="nav-link" href="#" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Calendar"><i class="ficon" data-feather="calendar"></i></a></li>
-          <li class="nav-item d-none d-lg-block"><a class="nav-link" href="#" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Todo"><i class="ficon" data-feather="check-square"></i></a></li>
-        </ul>
-        <ul class="nav navbar-nav">
-          <li class="nav-item d-none d-lg-block"><a class="nav-link bookmark-star"><i class="ficon text-warning" data-feather="star"></i></a>
-            <div class="bookmark-input search-input">
-              <div class="bookmark-input-icon"><i data-feather="search"></i></div>
-              <input class="form-control input" type="text" placeholder="Bookmark" tabindex="0" data-search="search">
-              <ul class="search-list search-list-bookmark"></ul>
-            </div>
-          </li>
-        </ul>
-      </div>
-      <ul class="nav navbar-nav align-items-center ms-auto">
-        <li class="nav-item dropdown dropdown-language"><a class="nav-link dropdown-toggle" id="dropdown-flag" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="flag-icon flag-icon-us"></i><span class="selected-language">English</span></a>
-          {{-- <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-flag"><a class="dropdown-item" href="#" data-language="en"><i class="flag-icon flag-icon-us"></i> English</a><a class="dropdown-item" href="#" data-language="fr"><i class="flag-icon flag-icon-fr"></i> French</a><a class="dropdown-item" href="#" data-language="de"><i class="flag-icon flag-icon-de"></i> German</a><a class="dropdown-item" href="#" data-language="pt"><i class="flag-icon flag-icon-pt"></i> Portuguese</a></div> --}}
-        </li>
-        <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-style"><i class="ficon" data-feather="moon"></i></a></li>
-        <li class="nav-item nav-search"><a class="nav-link nav-link-search"><i class="ficon" data-feather="search"></i></a>
-          <div class="search-input">
-            <div class="search-input-icon"><i data-feather="search"></i></div>
-            <input class="form-control input" type="text" placeholder="Search Herbo Vedik " tabindex="-1" data-search="search">
-            <div class="search-input-close"><i data-feather="x"></i></div>
-            <ul class="search-list search-list-main"></ul>
-          </div>
-        </li>
-        {{-- <li class="nav-item dropdown dropdown-cart me-25"><a class="nav-link" href="#" data-bs-toggle="dropdown"><i class="ficon" data-feather="shopping-cart"></i><span class="badge rounded-pill bg-primary badge-up cart-item-count">6</span></a>
+        <div class="bookmark-wrapper d-flex align-items-center">
+            <ul class="nav navbar-nav d-xl-none">
+                <li class="nav-item"><a class="nav-link menu-toggle" href="#"><i class="ficon"
+                            data-feather="menu"></i></a></li>
+            </ul>
+            <ul class="nav navbar-nav bookmark-icons">
+                <li class="nav-item d-none d-lg-block"><a class="nav-link" href="#" data-bs-toggle="tooltip"
+                        data-bs-placement="bottom" title="Email"><i class="ficon" data-feather="mail"></i></a></li>
+                <li class="nav-item d-none d-lg-block"><a class="nav-link" href="#" data-bs-toggle="tooltip"
+                        data-bs-placement="bottom" title="Chat"><i class="ficon"
+                            data-feather="message-square"></i></a></li>
+                <li class="nav-item d-none d-lg-block"><a class="nav-link" href="#" data-bs-toggle="tooltip"
+                        data-bs-placement="bottom" title="Calendar"><i class="ficon" data-feather="calendar"></i></a>
+                </li>
+                <li class="nav-item d-none d-lg-block"><a class="nav-link" href="#" data-bs-toggle="tooltip"
+                        data-bs-placement="bottom" title="Todo"><i class="ficon" data-feather="check-square"></i></a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav">
+                <li class="nav-item d-none d-lg-block"><a class="nav-link bookmark-star"><i class="ficon text-warning"
+                            data-feather="star"></i></a>
+                    <div class="bookmark-input search-input">
+                        <div class="bookmark-input-icon"><i data-feather="search"></i></div>
+                        <input class="form-control input" type="text" placeholder="Bookmark" tabindex="0"
+                            data-search="search">
+                        <ul class="search-list search-list-bookmark"></ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <ul class="nav navbar-nav align-items-center ms-auto">
+            <li class="nav-item dropdown dropdown-language"><a class="nav-link dropdown-toggle" id="dropdown-flag"
+                    href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i
+                        class="flag-icon flag-icon-us"></i><span class="selected-language">English</span></a>
+                {{-- <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-flag"><a class="dropdown-item" href="#" data-language="en"><i class="flag-icon flag-icon-us"></i> English</a><a class="dropdown-item" href="#" data-language="fr"><i class="flag-icon flag-icon-fr"></i> French</a><a class="dropdown-item" href="#" data-language="de"><i class="flag-icon flag-icon-de"></i> German</a><a class="dropdown-item" href="#" data-language="pt"><i class="flag-icon flag-icon-pt"></i> Portuguese</a></div> --}}
+            </li>
+            <li class="nav-item d-none d-lg-block"><a class="nav-link nav-link-style"><i class="ficon"
+                        data-feather="moon"></i></a></li>
+            <li class="nav-item nav-search"><a class="nav-link nav-link-search"><i class="ficon"
+                        data-feather="search"></i></a>
+                <div class="search-input">
+                    <div class="search-input-icon"><i data-feather="search"></i></div>
+                    <input class="form-control input" type="text" placeholder="Search Herbo Vedik " tabindex="-1"
+                        data-search="search">
+                    <div class="search-input-close"><i data-feather="x"></i></div>
+                    <ul class="search-list search-list-main"></ul>
+                </div>
+            </li>
+            {{-- <li class="nav-item dropdown dropdown-cart me-25"><a class="nav-link" href="#" data-bs-toggle="dropdown"><i class="ficon" data-feather="shopping-cart"></i><span class="badge rounded-pill bg-primary badge-up cart-item-count">6</span></a>
           <ul class="dropdown-menu dropdown-menu-media dropdown-menu-end">
             <li class="dropdown-menu-header">
               <div class="dropdown-header d-flex">
@@ -103,15 +119,17 @@
             </li>
           </ul>
         </li> --}}
-        <li class="nav-item dropdown dropdown-notification me-25"><a class="nav-link" href="#" data-bs-toggle="dropdown"><i class="ficon" data-feather="bell"></i><span class="badge rounded-pill bg-danger badge-up">5</span></a>
-          <ul class="dropdown-menu dropdown-menu-media dropdown-menu-end">
-            <li class="dropdown-menu-header">
-              <div class="dropdown-header d-flex">
-                <h4 class="notification-title mb-0 me-auto">Notifications</h4>
-                <div class="badge rounded-pill badge-light-primary">2 New</div>
-              </div>
-            </li>
-            {{-- <li class="scrollable-container media-list"><a class="d-flex" href="#">
+            <li class="nav-item dropdown dropdown-notification me-25"><a class="nav-link" href="#"
+                    data-bs-toggle="dropdown"><i class="ficon" data-feather="bell"></i><span
+                        class="badge rounded-pill bg-danger badge-up">5</span></a>
+                <ul class="dropdown-menu dropdown-menu-media dropdown-menu-end">
+                    <li class="dropdown-menu-header">
+                        <div class="dropdown-header d-flex">
+                            <h4 class="notification-title mb-0 me-auto">Notifications</h4>
+                            <div class="badge rounded-pill badge-light-primary">2 New</div>
+                        </div>
+                    </li>
+                    {{-- <li class="scrollable-container media-list"><a class="d-flex" href="#">
 
                 <div class="list-item d-flex align-items-start">
                   <div class="me-1">
@@ -169,19 +187,37 @@
                   </div>
                 </div></a>
             </li> --}}
-            <li class="dropdown-menu-footer"><a class="btn btn-primary w-100" href="#">Read all notifications</a></li>
-          </ul>
-        </li>
-        <li class="nav-item dropdown dropdown-user"><a class="nav-link dropdown-toggle dropdown-user-link" id="dropdown-user" href="#" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <div class="user-nav d-sm-flex d-none"><span class="user-name fw-bolder">BAAZ</span><span class="user-status">Admin</span></div><span class="avatar"><img class="round" src="{{asset('upload/bazz_logo.png')}}" alt="avatar" height="40" width="40"><span class="avatar-status-online"></span></span></a>
-          <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user"><a class="dropdown-item" href="#"><i class="me-50" data-feather="user"></i> Profile</a><a class="dropdown-item" href="#"><i class="me-50" data-feather="mail"></i> Inbox</a><a class="dropdown-item" href="app-todo.html"><i class="me-50" data-feather="check-square"></i> Task</a><a class="dropdown-item" href="#"><i class="me-50" data-feather="message-square"></i> Chats</a>
-            <div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="me-50" data-feather="settings"></i> Settings</a>{{--<a class="dropdown-item" href="page-pricing.html"><i class="me-50" data-feather="credit-card"></i> Pricing</a><a class="dropdown-item" href="page-faq.html"><i class="me-50" data-feather="help-circle"></i> FAQ</a>--}}
-            <form method="POST" action='{{route('Backend.logout')}}' id="my_form">
-              @csrf
-            <a class="dropdown-item" onclick="document.getElementById('my_form').submit();"><i class="me-50" data-feather="power"></i> Logout</a>
-            </form>
-          </div>
-        </li>
-      </ul>
+                    <li class="dropdown-menu-footer"><a class="btn btn-primary w-100" href="#">Read all
+                            notifications</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown dropdown-user"><a class="nav-link dropdown-toggle dropdown-user-link"
+                    id="dropdown-user" href="#" data-bs-toggle="dropdown" aria-haspopup="true"
+                    aria-expanded="false">
+                    <div class="user-nav d-sm-flex d-none">
+                        <span class="user-name fw-bolder">BAAZ</span>
+                        <span class="user-status">Admin</span>
+                    </div>
+                    <span class="avatar">
+                        <img class="round" alt="avatar" height="40" width="40" src="{{ asset('upload/bazz_logo.png') }}">
+                        <span class="avatar-status-online"></span>
+                    </span>
+                </a>
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user"><a class="dropdown-item"
+                        href="#"><i class="me-50" data-feather="user"></i> Profile</a><a
+                        class="dropdown-item" href="#"><i class="me-50" data-feather="mail"></i> Inbox</a><a
+                        class="dropdown-item" href="app-todo.html"><i class="me-50"
+                            data-feather="check-square"></i> Task</a><a class="dropdown-item" href="#"><i
+                            class="me-50" data-feather="message-square"></i> Chats</a>
+                    <div class="dropdown-divider"></div><a class="dropdown-item" href="#"><i class="me-50"
+                            data-feather="settings"></i> Settings</a>{{-- <a class="dropdown-item" href="page-pricing.html"><i class="me-50" data-feather="credit-card"></i> Pricing</a><a class="dropdown-item" href="page-faq.html"><i class="me-50" data-feather="help-circle"></i> FAQ</a> --}}
+                    <form method="POST" action='{{ route('Backend.logout') }}' id="my_form">
+                        @csrf
+                        <a class="dropdown-item" onclick="document.getElementById('my_form').submit();"><i
+                                class="me-50" data-feather="power"></i> Logout</a>
+                    </form>
+                </div>
+            </li>
+        </ul>
     </div>
-  </nav>
+</nav>
